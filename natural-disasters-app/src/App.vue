10ca54576568d8e91ceef97c794baa1b03aea243
Daniel Contreras <bones2995@hotmail.com>
2019-02-23 16:06:29 -0600

    InicializaciónDelProyectoVue

---
+++ natural-disasters-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <landing></landing>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Landing from "./components/Landing.vue";
+import Foot from "./components/Foot.vue";
+
+export default {
+    name: "app",
+    components: {
+        Navbar,
+        Landing,
+        Foot
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
